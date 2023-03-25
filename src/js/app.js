eb5075b4dcfcf62b43b9d3f6e3bbd95cf33f7c5a
Carolina-hora <carolina22hora@gmail.com>
2023-03-25 20:24:53 +0000

    Profile card with ternary operators and alert box when links are not filled properly

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,52 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${
+            variables.name === null
+              ? "First Name"
+              : variables.name.charAt(0).toUpperCase() +
+                variables.name.slice(1).toLowerCase()
+          } 
+          ${
+            variables.lastname === null
+              ? "Last Name"
+              : variables.lastname.charAt(0).toUpperCase() +
+                variables.lastname.slice(1).toLowerCase()
+          }
+          </h1>
+          <h2>${
+            variables.role === null ? "Choose your Role" : variables.role
+          }</h2>
+          <h3>${variables.city === null ? "Choose your City" : variables.city}, 
+          ${
+            variables.country === null
+              ? "Choose your Country"
+              : variables.country
+          }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${
+              variables.twitter === null
+                ? `javascript:void(alert('Please fill your Twitter account!'))`
+                : /^https?:\/\//.test(variables.twitter)
+                ? variables.twitter
+                : `javascript:void(alert('${variables.twitter} is not a valid URL!'))`
+            }">
+            <i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github === "alesanchezr"
+                ? `javascript:void(alert('Please fill your Github account!'))`
+                : variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin === null
+                ? `javascript:void(alert('Please fill your LinkedIn account!'))`
+                : variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram === null
+                ? `javascript:void(alert('Please fill your Instagram account!'))`
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
